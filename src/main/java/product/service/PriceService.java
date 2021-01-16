2b27fc5d138d983860bd6b6848cfb98f850c5594
amylokh <amylokh@gmail.com>
2021-01-16 21:22:40 +0530

    Implement retry mechanism for external api calls

--- src/main/java/product/service/PriceService.java
+++ src/main/java/product/service/PriceService.java
@@ -1,10 +1,11 @@
 package product.service;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
@@ -22,6 +23,7 @@ public class PriceService implements IPriceService {
     private RestTemplate restTemplate;
 
     @Override
+    @Retryable(maxAttempts=4, value = RuntimeException.class, backoff = @Backoff(delay = 15000, multiplier = 2))
     public Price retrieveCurrentExchangeRates() {
         System.out.println("Calling fixer API");
         final String fixerURL = "http://data.fixer.io/api/latest?access_key=c7c5a491cd074a819e1676dc47809873&symbols=USD,AUD,CAD,PLN,MXN,INR&format=1";
